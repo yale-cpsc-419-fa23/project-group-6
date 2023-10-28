083f0a2d42d980ec53adcbd6e2497cddacc76168
Kai Gao <87157271+Kaifeng-Gao@users.noreply.github.com>
2023-10-28 12:01:08 -0400

    Init app module

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+# from flask_login import LoginManager
 
-app = Flask(__name__, template_folder='app/views')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-db = SQLAlchemy(app)
 
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+db = SQLAlchemy()
+def create_app(config_name):
+    app = Flask(__name__, template_folder='app/views')
+    # Load configurations from config.py
+    app.config.from_object(f"app.config.{config_name}")
+    # Initialize extensions with app
+    db.init_app(app)
+
+    # login_manager = LoginManager(app)
+    # login_manager.login_view = 'login'
+
+    return app
