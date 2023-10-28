5bbd4be32aaf05d0bfb66baf99416c66b851c97d
Kai Gao <87157271+Kaifeng-Gao@users.noreply.github.com>
2023-10-28 01:27:57 -0400

    Implement model for users

--- app/models/user.py
+++ app/models/user.py
@@ -1,15 +1,62 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
-from app import db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../../music_data.sqlite'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    userId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    gender = db.Column(db.String(10), nullable=False)
+    birthday = db.Column(db.Date, nullable=False)
+    registeredDateTime = db.Column(db.DateTime, nullable=False)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+    def __repr__(self):
+        return f"<User {self.username}>"
 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+    @classmethod
+    def create(cls, username, password, email, gender, birthday):
+        new_user = cls(username=username, password=password, email=email, gender=gender, birthday=birthday, registerdDateTime=datetime())
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user
+
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def find_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
+
+    @classmethod
+    def find_by_id(cls, user_id):
+        return db.session.get(cls, user_id)
+
+    @classmethod
+    def alter(cls, user_id, username, password, email, gender, birthday):
+        user = db.session.get(cls, user_id)
+        if not user:
+            return None
+        if username:
+            user.username = username
+        if password:
+            user.password = password
+        if email:
+            user.email = email
+        if gender:
+            user.gender = gender
+        if birthday:
+            user.birthday = birthday
+        db.session.commit()
+        return user
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        U = User()
+        print(U.find_by_id(100))
