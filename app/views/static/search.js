c920503551d157d8d92fb7673d2b11a15e933b83
Kai Gao <87157271+Kaifeng-Gao@users.noreply.github.com>
2023-12-03 22:03:59 -0500

    Implement search history using cookie

--- app/views/static/search.js
+++ app/views/static/search.js
@@ -3,6 +3,37 @@ $(document).ready(function() {
     const resultsDiv = $("#search-results tbody");
     const searchButton = $("#search-button");
 
+
+    function setCookie(name, value, days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    }
+
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
+
+    function autoSearchWithLastQuery() {
+        var lastSearchQuery = getCookie('lastSearchQuery');
+        if (lastSearchQuery) {
+            performSearch(lastSearchQuery, true);
+        }
+    }
+
+    autoSearchWithLastQuery();
+
     searchInput.on("keyup", function(e) {
         const query = $(this).val();
         const isEnterPressed = e.which === 13;
@@ -58,6 +89,7 @@ $(document).ready(function() {
         $.get("/search", { song_name: query, increment_popularity: incrementPopularity }, function(data) {
             displaySearchResults(data, incrementPopularity);
         });
+        setCookie('lastSearchQuery', query, 7);
     }
 
     function displaySearchResults(data, incrementPopularity) {
